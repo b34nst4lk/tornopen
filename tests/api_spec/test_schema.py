b0bcb1236ab0d9765f7118c416b1c51353298b96
Jack Sim <jack@99.co>
2021-12-19 00:26:25 +0800

    test: fixed broken test for adding duplicate schemas to apispec

--- tests/api_spec/test_schema.py
+++ tests/api_spec/test_schema.py
@@ -6,13 +6,12 @@ from pydantic import BaseModel
 from torn_open.web import Application, AnnotatedHandler
 from torn_open.models import RequestModel, ResponseModel
 
-# from tests.api_spec.schema_util import SharedSchema1Handler
+def test_create_app_with_duplicated_schemas():
+    class DoublyNestedModel(BaseModel):
+        key: str
 
-
-@pytest.fixture
-def app():
     class NestedModel(BaseModel):
-        nested_field: str
+        doubly_nested_model: DoublyNestedModel
 
     class SharedRequestModel(RequestModel):
         nested_model: NestedModel
@@ -27,21 +26,17 @@ def app():
     class Schema2Handler(AnnotatedHandler):
         def post(self, req_model: SharedRequestModel) -> SharedResponseModel:
             pass
+    
+    class Schema3Handler(AnnotatedHandler):
+        def post(self, req_model: SharedRequestModel) -> SharedRequestModel:
+            pass
+
 
     return Application(
         [
             url(r"/1", Schema1Handler),
             url(r"/1/duplicated", Schema1Handler),
             url(r"/2", Schema2Handler),
+            url(r"/3", Schema3Handler),
         ]
     )
-
-
-@pytest.fixture
-def spec(app):
-    return app.api_spec.to_dict()
-
-
-def test_shared_schema(spec):
-    # This test should fail if APISpec is unable to handle models shared by multiple handlers
-    pass
